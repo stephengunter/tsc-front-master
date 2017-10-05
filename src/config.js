df9950391e78a930aa8247cb9ac98a2fdae3a967
stephengunter <traders.com.tw@gmail.com>
2017-10-05 13:15:22 +0800

    20171005-7

--- src/config.js
+++ src/config.js
@@ -1,16 +1,16 @@
 class Config {
     static siteUrl(){
-         //return 'http://tsc'//'http://localhost:8080' 
+        // return 'http://tsc'//'http://localhost:8080' 
          return 'http://203.64.37.90' 
     }
     static apiUrl(){
-        // return 'http://tsc-master'
+         //return 'http://tsc-master'
         return 'http://203.64.37.41:8080' 
     }
     static apiClientSettings() {
         return {
             id:2,
-            secret:'AzUbjsNJMkKvLBrI59d4hOVxdQDVxma35i2EvHjH'
+            secret:'3Ax5Q96ufbbqeSdrLQ9Mnk1MFwVvdGWNVtEMOlFo'
         }
     }
 
